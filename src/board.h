e1514586bc924f7a5a1a40c45ab35b5dcbd924e2
Matt <mattnappo@gmail.com>
2019-07-29 15:25:26 -0400

    Wrote piece type.

--- src/board.h
+++ src/board.h
@@ -1,8 +1,35 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-
 namespace pf {
+
+enum piece_color {
+    BLACK,
+    WHITE
+};
+
+enum piece_type {
+    PUSHER,
+    PAWN
+};
+
+class Piece {
+public:
+    Piece(piece_color color, piece_type type, int x, int y);
+    
+    int get_x();
+    int get_y();
+
+    piece_color get_piece_color();
+    piece_type get_piece_type();
+
+private:
+    int x;
+    int y;
+
+    piece_color color;    
+    piece_type type;
+};
 
 class Square {
 public:
